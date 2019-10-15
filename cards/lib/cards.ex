a43e750e8dabe8d6325429c0c0afc5972d8ca15f
Adriano Santin <adr.santin@gmail.com>
2019-10-15 08:55:51 -0300

    Initial commit

---
+++ cards/lib/cards.ex
@@ -0,0 +1,27 @@
+# Module = collection of methods or functions
+defmodule Cards do
+
+  # Returns last value inside method
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+    "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+end
