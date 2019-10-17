7ec1c62947e77c6fc6438b0c8efb8208bd9b45bd
Adriano Santin <adr.santin@gmail.com>
2019-10-17 09:25:39 -0300

    Add some documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,7 +1,13 @@
 # Module = collection of methods or functions
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
 
   # Returns last value inside method
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
     "Ten", "Jack", "Queen", "King"]
@@ -20,6 +26,18 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should 
+    be in the hand
+
+  ## Examples
+  
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -38,4 +56,11 @@ defmodule Cards do
     end
   end
   
+  # Use consistent first arguments for Pipe operator
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
 end
