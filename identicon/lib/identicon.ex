d7adf673db684c561a1730140ae19b601ca545d3
Adriano Santin <adr.santin@gmail.com>
2019-10-23 14:08:07 -0300

    Create image grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,16 +4,34 @@ defmodule Identicon do
     input 
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
-    [r, g, b]
+  #Can pattern match on parameter of function
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid = 
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+    
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row([first, second | _tail] = row) do
+    # [145, 46, 200] -> [145, 46, 200, 46, 145]
+    row ++ [second, first]
   end
 end
